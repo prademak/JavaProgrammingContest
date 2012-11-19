3eac36fcea60e7ebfec917b7497072e2ee553198
Martijn van den Enk <martijnvandenenk@gmail.com>
2012-11-19 14:37:08 +0100

    initial commit

---
+++ JavaProgrammingContest.DataAccess/IRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JavaProgrammingContest.DataAccess
+{
+    public interface IRepository<T>
+    {
+        IQueryable<T> GetAll();
+        T GetById(int id);
+    }
+
+    public class GenericRepository<T> : IRepository<T> {
+        public IQueryable<T> GetAll() {
+            return null;
+        }
+
+        public T GetById(int id) {
+            return null;
+        }
+    }
+}
