acfd0ff8cb602918076c253f86eaabe812f11d0c
Martijn van den Enk <martijnvandenenk@gmail.com>
2012-11-22 14:36:11 +0100

    added context and dependency injection setup

---
+++ JavaProgrammingContest.DataAccess/Context/JavaProgrammingContestContext.cs
@@ -0,0 +1,15 @@
+﻿using System.Data.Entity;
+using JavaProgrammingContest.Domain.Entities;
+
+namespace JavaProgrammingContest.DataAccess.Context{
+    public class JavaProgrammingContestContext : DbContext{
+        public JavaProgrammingContestContext()
+            : base("JavaProgrammingContestDB") {}
+
+        public IDbSet<Contest> Contests { get; set; }
+        public IDbSet<Assignment> Assignments { get; set; }
+        public IDbSet<Participant> Participants { get; set; }
+        public IDbSet<Score> Scores { get; set; }
+        public IDbSet<Result> Results { get; set; }
+    }
+}
---
+++ JavaProgrammingContest.DataAccess/Context/JavaProgrammingContestContextInitializer.cs
@@ -0,0 +1,5 @@
+﻿using System.Data.Entity;
+
+namespace JavaProgrammingContest.DataAccess.Context{
+    public class JavaProgrammingContestContextInitializer : DropCreateDatabaseIfModelChanges<JavaProgrammingContestContext> {}
+}
