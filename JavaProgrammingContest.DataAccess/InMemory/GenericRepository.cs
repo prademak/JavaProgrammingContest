de4b7ef437182d2310a5145b951cc76f1f2862a8
Alexander Niketic <alexander.niketic@gmail.com>
2012-11-22 11:17:11 +0100

    InMemory toegevoegd
    GenericRepository voor InMemory

---
+++ JavaProgrammingContest.DataAccess/InMemory/GenericRepository.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+
+namespace JavaProgrammingContest.DataAccess.InMemory{
+    public class GenericRepository<T> : IRepository<T> where T : class
+    {
+        private List<T> _db;
+
+        public GenericRepository()
+        {
+            _db = new List<T>();
+        }
+
+        public IQueryable<T> GetAll()
+        {
+            return _db.AsQueryable();
+        }
+
+        public T GetById(int id)
+        {
+            return _db.ElementAt(id);
+        }
+
+        public void Add(T entity)
+        {
+            _db.Add(entity);
+        }
+
+        public void Remove(T entity)
+        {
+            _db.Remove(entity);
+        }
+
+        public void SaveChanges()
+        {
+        }
+    }
+}
