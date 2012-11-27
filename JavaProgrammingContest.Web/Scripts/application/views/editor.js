0628ef13c60b8516b9e91e0165d7e338af82b8db
Jeffrey van Harn <jeffrey.wizkid@gmail.com>
2012-11-27 10:24:01 +0100

    Added a MVVC implementation of the application.

---
+++ JavaProgrammingContest.Web/Scripts/application/views/editor.js
@@ -0,0 +1,54 @@
+﻿var EditorView;
+
+$(document).ready(function () {
+    EditorView = Backbone.View.extend({
+        // Representing element
+        el: $("#ContestEditor"),
+
+        // Default CodeMirror Options
+        defaultEditorOptions: {
+            mode: "text/x-java",
+            theme: "eclipse",
+            value: "// No assignment loaded yet.",
+    
+            lineNumbers: true,
+            lineWrapping: false,
+            gutter: true,
+            fixedGutter: true,
+
+            tabSize: 4,
+            indentUnit: 4,
+            indentWithTabs: true,
+            smartIndent: true,
+    
+            electricChars: true,
+            matchBrackets: true
+        },
+        
+        // CodeMirror Instance
+        codeMirror: null,
+        
+        // 
+        currentAssignment: null,
+
+        events: {
+            //"keypress #new-todo": "createOnEnter"
+        },
+
+        initialize: function () {
+            this.codeMirror = new CodeMirror(this.el, this.defaultEditorOptions);
+        },
+        
+        render: function () {
+            
+        },
+        
+        setContent: function(content) {
+            return this.codeMirror.setValue(content);
+        },
+
+        setOption: function(option, value) {
+            this.codeMirror.setOption(option, value);
+        }
+    });
+});
