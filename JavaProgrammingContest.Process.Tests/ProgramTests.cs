f967d857792e1a71760a9de3a41f10e0e50c1c21
Vincent de Jager <vincent.dejager@gmail.com>
2012-11-27 16:10:39 +0100

    Test Added

---
+++ JavaProgrammingContest.Process.Tests/ProgramTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using JavaProgrammingContest.Process;
+namespace JavaProgrammingContest.Process.Tests
+{
+    [TestFixture]
+    public class CompilerTests
+    {
+        Compiler _compiler;
+        Runner _runner;
+ 
+
+        [SetUp]
+        public void Init()
+        {
+            _compiler = new Compiler();
+            _runner = new Runner();
+           
+        }
+
+        [Test]
+        public void TestIfCompilerWorks()
+        {
+            Assert.IsNotNull(_compiler);
+          
+        }
+
+       
+    }
+}
