dad7e2b138a84e3896af5205de5c5614a4b9c6c3
Jeffrey van Harn <jeffrey.wizkid@gmail.com>
2012-12-04 14:28:55 +0100

    Some basic test coverage for assignments
    Work in process.

---
+++ JavaProgrammingContest.Web/Controllers/TestSuiteController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace JavaProgrammingContest.Web.Controllers
+{
+    public class TestSuiteController : Controller
+    {
+        //
+        // GET: /TestSuite/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
