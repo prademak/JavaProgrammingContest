dccf5baa6e5f71acacd393ac578cea712f4c0bf3
Martijn van den Enk <martijnvandenenk@gmail.com>
2012-12-04 15:18:14 +0100

    fixed compiler

--- JavaProgrammingContest.Web/API/BuildController.cs
+++ JavaProgrammingContest.Web/API/BuildController.cs
@@ -1,25 +1,31 @@
-﻿using System.Net;
+﻿using System;
+using System.Diagnostics;
+using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using System.Web.Http.ModelBinding;
+using System.Web.Script.Serialization;
 using System.Web.Security;
 using System.Web.UI;
 using JavaProgrammingContest.Process.Compiler;
+using NUnit.Framework;
 
-namespace JavaProgrammingContest.Web.API{
-    public class BuildController : ApiController{
+namespace JavaProgrammingContest.Web.API {
+    public class BuildController : ApiController {
         private readonly ICompiler _compiler;
 
-        public BuildController(ICompiler compiler){
+        public BuildController(ICompiler compiler) {
             _compiler = compiler;
         }
 
-        public HttpResponseMessage Post(BuildJob buildJob){
-            var user = Membership.GetUser(User.Identity.Name);
+        [HttpPost]
+        public HttpResponseMessage Post(BuildJob buildJob) {
+            //var user = Membership.GetUser(User.Identity.Name);
             var result = _compiler.CompileFromPlainText(buildJob.Code);
-            
+
             //TODO use CompilerResult class as response
             return Request.CreateResponse(HttpStatusCode.Created,
-                new BuildResult{
+                new BuildResult {
                     Output = result.StandardOutput,
                     Error = result.StandardError,
                     CompileTime = result.CompilationTime
@@ -27,11 +33,12 @@ namespace JavaProgrammingContest.Web.API{
         }
     }
 
-    public class BuildJob{
+    [Serializable]
+    public class BuildJob {
         public string Code { get; set; }
     }
 
-    public class BuildResult{
+    public class BuildResult {
         public int CompileTime { get; set; }
         public string Error { get; set; }
         public string Output { get; set; }
