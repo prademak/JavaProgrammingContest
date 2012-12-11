9b2544e8379edef06fbe223311a29865fdf8b6f9
Vincent de Jager <vincent.dejager@gmail.com>
2012-12-11 14:05:13 +0100

    Seed changed

--- JavaProgrammingContest.DataAccess/Migrations/Configuration.cs
+++ JavaProgrammingContest.DataAccess/Migrations/Configuration.cs
@@ -2,15 +2,20 @@ using System.Data.Entity.Migrations;
 using System.Linq;
 using System.Web.Security;
 using JavaProgrammingContest.DataAccess.Context;
+using JavaProgrammingContest.Domain.Entities;
 using WebMatrix.WebData;
 
-namespace JavaProgrammingContest.DataAccess.Migrations{
-    internal sealed class Configuration : DbMigrationsConfiguration<JavaProgrammingContestContext>{
-        public Configuration(){
+namespace JavaProgrammingContest.DataAccess.Migrations
+{
+    internal sealed class Configuration : DbMigrationsConfiguration<JavaProgrammingContestContext>
+    {
+        public Configuration()
+        {
             AutomaticMigrationsEnabled = true;
         }
 
-        protected override void Seed(JavaProgrammingContestContext context){
+        protected override void Seed(JavaProgrammingContestContext context)
+        {
             WebSecurity.InitializeDatabaseConnection(
                 "JavaProgrammingContest",
                 "Participants",
@@ -24,10 +29,40 @@ namespace JavaProgrammingContest.DataAccess.Migrations{
                 WebSecurity.CreateUserAndAccount(
                     "johndoe@mail.com",
                     "testing",
-                    new{Interested = false});
+                    new { Interested = false });
 
             if (!Roles.GetRolesForUser("johndoe@mail.com").Contains("Administrator"))
-                Roles.AddUsersToRoles(new[]{"johndoe@mail.com"}, new[]{"Administrator"});
+                Roles.AddUsersToRoles(new[] { "johndoe@mail.com" }, new[] { "Administrator" });
+
+            var ass1 = new Assignment
+                {
+                    Id = 1,
+                    CodeGiven =
+                        "// Sample class\nclass HelloWorldApp {\n\tpublic static void main(String[] args){\n\t\tSystem.out.println(\"Hello World!\");\n\t}\n}\n",
+                    Description =
+                        "Print the following text: Hello,World!",
+                    Title = "Hello, Word!",
+                    MaxSolveTime = 900
+                };
+            var ass2 = new Assignment
+            {
+                Id = 2,
+                CodeGiven =
+                    "// Sample class\nclass HelloWorldApp {\n\tpublic static void main(String[] args){\n\t\tSystem.out.println(\"Hello World!\");\n\t}\n}\n",
+                Description =
+                    "Print like: '2 3 5 7 11'",
+                Title = "Print 30 primenumbers",
+                MaxSolveTime = 900
+            };
+            var contest = new Contest { Id = 1, IsActive = true, Name = "Info Support Contest December" };
+
+            var contestAssignment = new ContestAssignment { Assignment = ass1, Contest = contest };
+            var contestAssignment2 = new ContestAssignment { Assignment = ass2, Contest = contest };
+
+            context.ContestAssignments.AddOrUpdate(contestAssignment);
+            context.ContestAssignments.AddOrUpdate(contestAssignment2);
+
+            context.SaveChanges();
         }
     }
 }
