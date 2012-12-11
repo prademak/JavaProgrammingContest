2457ee30d785402f3621fc99a09654dc31b18744
Martijn van den Enk <martijnvandenenk@gmail.com>
2012-12-11 14:01:04 +0100

    added progress api and fixed assignments controller

--- JavaProgrammingContest.Web/API/AssignmentsController.cs
+++ JavaProgrammingContest.Web/API/AssignmentsController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
@@ -15,24 +16,7 @@ namespace JavaProgrammingContest.Web.API{
         }
 
         public IEnumerable<Assignment> Get(){
-            return new[]{
-                new Assignment{
-                    Id = 1,
-                    CodeGiven = "// Sample class\nclass HelloWorldApp {\n\tpublic static void main(String[] args){\n\t\tSystem.out.println(\"Hello World!\");\n\t}\n}\n",
-                    Description = "Nullam ac venenatis arcu. Curabitur vitae malesuada sapien. Nam cursus, odio eget mollis rutrum, arcu ipsum pharetra diam, quis rhoncus magna sem non augue. Sed mollis rutrum dui, sed consequat ipsum congue eu. In luctus, orci id semper vehicula, neque lectus tristique lectus, eu interdum risus dolor non erat. Nullam ipsum eros, dignissim ac cursus non, ultrices vitae leo. Pellentesque mollis nisi ut orci euismod ac gravida magna aliquet. Aenean mi urna, fermentum ac lobortis condimentum, tincidunt in leo.",
-                    Title = "This is a sample assignment",
-                    MaxSolveTime = 900
-                },
-                new Assignment{
-                    Id = 2,
-                    CodeGiven = "",
-                    Description = "Quis rhoncus magna sem non augue. Sed mollis rutrum dui, sed consequat ipsum congue eu. In luctus, orci id semper vehicula, neque lectus tristique lectus, eu interdum risus dolor non erat. Nullam ipsum eros, dignissim ac cursus non, ultrices vitae leo. Pellentesque mollis nisi ut orci euismod ac gravida magna aliquet. Aenean mi urna, fermentum ac lobortis condimentum, tincidunt in leo.",
-                    Title = "Another awesome assignment",
-                    MaxSolveTime = 900
-                }
-            };
-
-            // TODO: Schijf geef terug _context dot Assignments
+            return _context.Assignments;
         }
 
         public Assignment Get(int id){
