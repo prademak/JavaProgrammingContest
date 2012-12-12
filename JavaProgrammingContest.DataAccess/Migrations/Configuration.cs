4262c98a4f4ebf97749605c92409772fa104ef50
Vincent de Jager <vincent.dejager@gmail.com>
2012-12-12 19:58:45 +0100

    Seed aangepast voor presentatie

--- JavaProgrammingContest.DataAccess/Migrations/Configuration.cs
+++ JavaProgrammingContest.DataAccess/Migrations/Configuration.cs
@@ -38,7 +38,19 @@ namespace JavaProgrammingContest.DataAccess.Migrations{
         }
 
         private static void InsertSampleData(JavaProgrammingContestContext context){
+        
             var ass1 = new Assignment{
+                CodeGiven =
+                    "// Sample class\nclass HelloWorldApp {\n\tpublic static void main(String[] args){\n\t\tSystem.out.println(\"Hello World!\");\n\t}\n}\n",
+                Description =
+                    "Print like: '2 3 5 7 11'",
+                Title = "Print 30 primenumbers",
+                MaxSolveTime = 900,
+                RunCodeInput = "",
+                RunCodeOuput = "2 3 5 7 11 13 17 19 23 29 31 37 41 43 47 53 59 61 67 71 73 79 83 89 97 101 103 107 109 113"
+            };
+            var ass2 = new Assignment
+            {
                 CodeGiven =
                     "// Sample class\nclass HelloWorldApp {\n\tpublic static void main(String[] args){\n\t\tSystem.out.println(\"Hello World!\");\n\t}\n}\n",
                 Description =
@@ -48,17 +60,6 @@ namespace JavaProgrammingContest.DataAccess.Migrations{
                 RunCodeInput = "",
                 RunCodeOuput = "Hello World!"
             };
-            var ass2 = new Assignment{
-                CodeGiven =
-                    "// Sample class\nclass HelloWorldApp {\n\tpublic static void main(String[] args){\n\t\tSystem.out.println(\"Hello World!\");\n\t}\n}\n",
-                Description =
-                    "Print like: '2 3 5 7 11'",
-                Title = "Print 30 primenumbers",
-                MaxSolveTime = 900,
-                RunCodeInput = "",
-                RunCodeOuput = "Hello World!"
-            };
-
             var contest = new Contest{
                 IsActive = true,
                 Name = "Info Support Contest December",
