0d7f0cf9ff472a574ceafcf32f307c906d983a23
Vincent de Jager <vincent.dejager@gmail.com>
2012-12-17 14:33:20 +0100

    AssignmentTest

--- JavaProgrammingContest.Web.Tests/Controllers/AssignmentControllerTests.cs
+++ JavaProgrammingContest.Web.Tests/Controllers/AssignmentControllerTests.cs
@@ -1,6 +1,32 @@
-﻿using NUnit.Framework;
+﻿using System;
+using System.Web.Mvc;
+using JavaProgrammingContest.DataAccess.Context;
+using JavaProgrammingContest.Domain.Entities;
+using JavaProgrammingContest.Web.Controllers;
+using Moq;
+using NUnit.Framework;
 
 namespace JavaProgrammingContest.Web.Tests.Controllers{
     [TestFixture]
-    public class AssignmentControllerTests {}
+    public class AssignmentControllerTests {
+        private AssignmentController _controller;
+        private Mock<IDbContext> _contextMock;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _contextMock = new Mock<IDbContext>();
+         
+            _controller = new AssignmentController(_contextMock.Object);
+        }
+
+        [Test]
+        public void AssignmentControllerReturnsAView()
+        {
+        
+
+         
+       //    Assert.NotNull(result); 
+        }
+    }
 }
