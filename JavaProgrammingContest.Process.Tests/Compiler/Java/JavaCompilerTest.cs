0f79e0c279a4232de93b357b406250aee42a74b0
Vincent de Jager <vincent.dejager@gmail.com>
2012-12-17 13:40:02 +0100

    javacompiler uses unique dir

--- JavaProgrammingContest.Process.Tests/Compiler/Java/JavaCompilerTest.cs
+++ JavaProgrammingContest.Process.Tests/Compiler/Java/JavaCompilerTest.cs
@@ -1,8 +1,26 @@
-﻿using NUnit.Framework;
+﻿using System;
+using JavaProgrammingContest.Domain.Entities;
+using JavaProgrammingContest.Process.Compiler.Java;
+using NUnit.Framework;
 
-namespace JavaProgrammingContest.Process.Tests.Compiler.Java{
+namespace JavaProgrammingContest.Process.Tests.Compiler.Java
+{
     [TestFixture]
-    public class JavaCompilerTests{
+    public class JavaCompilerTests
+    {
+        private JavaCompiler jc; 
+        [SetUp]
+        public void SetUp()
+        {
+            jc = new JavaCompiler();
+        }
 
+        [TestCase]
+        [ExpectedException(typeof(NullReferenceException ))]
+        public void CreateFilePathGivesNullReferenceException()
+        {
+            jc = new JavaCompiler();
+            String result = jc.CreateFilePath(1, "HelloWorldApp");
+        }
     }
 }
--- JavaProgrammingContest.Process/Compiler/Java/JavaCompiler.cs
+++ JavaProgrammingContest.Process/Compiler/Java/JavaCompiler.cs
@@ -9,16 +9,18 @@ namespace JavaProgrammingContest.Process.Compiler.Java{
         public ICompilerProcess CompilerProcess { get; set; }
 
         public CompilerResult CompileFromPlainText(Participant participant, string code){
-            var javaFile = CreateFilePath(participant, "HelloWorldApp");
+            var javaFile = CreateFilePath(participant.Id, "HelloWorldApp");
             File.WriteAllText(javaFile, code);
             return CompilerProcess.Compile(string.Format("\"{0}\"", javaFile));
         }
 
-        public string CreateFilePath(Participant participant, string appName){
+        public string CreateFilePath(int id, string appName){
             //TODO currentPath will possibly not work when app is published
-
+           
             var currentPath = HttpContext.Current.Server.MapPath("~/");
-
+            currentPath = Path.Combine(currentPath, "temp");
+          currentPath = Path.Combine(currentPath, id.ToString());
+           Directory.CreateDirectory(currentPath);
             var fileName = appName + ".java";
             return Path.Combine(currentPath, fileName);
         }
