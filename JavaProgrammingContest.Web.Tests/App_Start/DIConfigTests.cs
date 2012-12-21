2f73e68eafa26f7807e22ed6984e06fd70fa567a
Jeffrey van Harn <jeffrey.wizkid@gmail.com>
2012-12-21 11:49:01 +0100

    Working UserSetting tests.

---
+++ JavaProgrammingContest.Web.Tests/App_Start/DIConfigTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace JavaProgrammingContest.Web.Tests.App_Start
+{
+    [TestFixture]
+    class DIConfigTests
+    {
+        [Test]
+        public void CheckObjectErrors()
+        {
+            DIConfig.RegisterTypes();
+            Assert.IsTrue(true);
+        }
+    }
+}
