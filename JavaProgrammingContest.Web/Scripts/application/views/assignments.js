effafa5f62ae0e3aa49bca0ffdc4ac5a10fdd91a
Jeffrey van Harn <jeffrey.wizkid@gmail.com>
2013-01-07 12:21:12 +0100

    Space

--- JavaProgrammingContest.Web/Scripts/application/views/assignments.js
+++ JavaProgrammingContest.Web/Scripts/application/views/assignments.js
@@ -109,6 +109,7 @@ $(document).ready(function () {
             ass.find('h1').text(this.current.get('Title'));
             ass.find('.description').text(this.current.get('Description'));
             ass.find('.time').text(Math.round(this.current.get('MaxSolveTime') / 60) + ' minutes');
+
             ass = $('#StartPane');
             ass.find('h1').text("Assignment " + this.current.get('Id'));
             ass.find('.description').text("This assignment has and estimated maximal timespan of "+Math.round(this.current.get('MaxSolveTime') / 60) + ' minutes. Start the time, to see the assignment and write code.');
