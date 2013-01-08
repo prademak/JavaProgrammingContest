27dc32e452011661714bf3497e074493533cce01
Martijn van den Enk <martijnvandenenk@gmail.com>
2013-01-08 12:54:45 +0100

    cleanup

--- JavaProgrammingContest.DataAccess/TestSupport/FakeUserSettingsSet.cs
+++ JavaProgrammingContest.DataAccess/TestSupport/FakeUserSettingsSet.cs
@@ -1,7 +1,7 @@
 ﻿using System.Linq;
 using JavaProgrammingContest.Domain.Entities;
 
-namespace JavaProgrammingContest.DataAccess.Context.TestSupport{
+namespace JavaProgrammingContest.DataAccess.TestSupport{
     public class FakeUserSettingsSet : FakeDbSet<UserSetting>{
         public override UserSetting Find(params object[] keyValues){
             return Local.SingleOrDefault(a => a.Id == (int) keyValues[0]);
