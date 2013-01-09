aadc1555dbba69114c2a91e75dcc1c7401d43a27
Vincent de Jager <vincent.dejager@gmail.com>
2013-01-09 16:30:01 +0100

    linkedin oauth

--- JavaProgrammingContest.Web/App_Start/AuthConfig.cs
+++ JavaProgrammingContest.Web/App_Start/AuthConfig.cs
@@ -3,7 +3,7 @@
 namespace JavaProgrammingContest.Web.App_Start {
     public static class AuthConfig {
         public static void RegisterAuth() {
-         
+         /*
             OAuthWebSecurity.RegisterTwitterClient(
               consumerKey: "YWIn1H8KULGObNr44RbBw",
                 consumerSecret: "awZspGQZhvh4zNyydT7WfVC5VfM4M2Fua5UP4D4Ak");
@@ -12,7 +12,9 @@ namespace JavaProgrammingContest.Web.App_Start {
                 appId: "179944392143624",
                 appSecret: "f3822e9fd6e2ac05f53d157e48567009");
 
-             OAuthWebSecurity.RegisterGoogleClient();
+             OAuthWebSecurity.RegisterGoogleClient();*/
+            OAuthWebSecurity.RegisterLinkedInClient(consumerKey: "b08e9cc9-a08e-4842-a702-46fc5527905f", consumerSecret: "54044dc3-35e8-4fb4-8f44-ff74ad7fc1be");
+
         }
     }
 }
