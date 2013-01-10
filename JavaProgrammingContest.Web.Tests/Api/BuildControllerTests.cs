a8f714ea0cb040a812c1ad41e6fa9bdd10c93856
Vincent de Jager <vincent.dejager@gmail.com>
2013-01-10 15:46:22 +0100

    BuildTest

--- JavaProgrammingContest.Web.Tests/Api/BuildControllerTests.cs
+++ JavaProgrammingContest.Web.Tests/Api/BuildControllerTests.cs
@@ -1,6 +1,36 @@
-﻿using NUnit.Framework;
+﻿using JavaProgrammingContest.DataAccess.Context;
+using JavaProgrammingContest.Process.Compiler;
+using JavaProgrammingContest.Process.Runner;
+using JavaProgrammingContest.Web.API;
+using JavaProgrammingContest.Web.App_Start;
+using Moq;
+using NUnit.Framework;
 
 namespace JavaProgrammingContest.Web.Tests.Api{
     [TestFixture]
-    public class BuildControllerTests {}
+    public class BuildControllerTests {
+        private BuildController _controller;
+        private Mock<IDbContext> _contextMock;
+        private static IRunner _runner;
+        private static ICompiler _compiler;
+        private BuildController.BuildJob _buildjob;
+        [SetUp]
+        public void SetUp()
+        {
+            _contextMock = new Mock<IDbContext>(); 
+            MapperConfig.Configure();
+            _compiler = new TestCompiler();
+            _controller = new BuildController(_contextMock.Object, _compiler);
+            _buildjob = new BuildController.BuildJob{ Code="test" };
+
+        }
+
+        [Test]
+        public void GetUserSettingsWithValidId()
+        {
+            Assert.AreEqual("Hello World!", _controller.Post(_buildjob));
+          
+        }
+    
+    }
 }
