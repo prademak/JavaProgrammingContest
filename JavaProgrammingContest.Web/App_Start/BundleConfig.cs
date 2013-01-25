ab845613b98b60d559f38f62afb4d7df324edf8e
Martijn van den Enk <martijnvandenenk@gmail.com>
2013-01-25 11:43:14 +0100

    bundleconfig

--- JavaProgrammingContest.Web/App_Start/BundleConfig.cs
+++ JavaProgrammingContest.Web/App_Start/BundleConfig.cs
@@ -1,30 +1,30 @@
 ﻿using System.Web.Optimization;
 
-namespace JavaProgrammingContest.Web.App_Start {
+namespace JavaProgrammingContest.Web.App_Start{
     /// <summary>
     ///     Internal Class.
     /// </summary>
-    public static class BundleConfig {
+    public static class BundleConfig{
         /// <summary>
         ///     Bundles all Javascript etc in one file and compresses them.
         ///     For more information on Bundling, visit http://go.microsoft.com/fwlink/?LinkId=254725
         /// </summary>
         /// <param name="bundles"></param>
-        public static void RegisterBundles(BundleCollection bundles) {
+        public static void RegisterBundles(BundleCollection bundles){
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                "~/Scripts/jquery-{version}.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
-                        "~/Scripts/jquery-ui-{version}.js"));
+                "~/Scripts/jquery-ui-{version}.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.unobtrusive*",
-                        "~/Scripts/jquery.validate*"));
+                "~/Scripts/jquery.unobtrusive*",
+                "~/Scripts/jquery.validate*"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
+                "~/Scripts/modernizr-*"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/style.css"));
 
@@ -34,12 +34,11 @@ namespace JavaProgrammingContest.Web.App_Start {
 
             // CodeMirror Files
             bundles.Add(new StyleBundle("~/Content/CodeMirror").Include(
-                        "~/Scripts/codemirror-2.35/lib/codemirror.css",
-                        "~/Content/editor.css",
-
-                        "~/Scripts/codemirror-2.35/theme/eclipse.css",
-                        "~/Scripts/codemirror-2.35/theme/lesser-dark.css",
-                        "~/Scripts/codemirror-2.35/theme/monokai.css"));
+                "~/Scripts/codemirror-2.35/lib/codemirror.css",
+                "~/Content/editor.css",
+                "~/Scripts/codemirror-2.35/theme/eclipse.css",
+                "~/Scripts/codemirror-2.35/theme/lesser-dark.css",
+                "~/Scripts/codemirror-2.35/theme/monokai.css"));
 
             bundles.Add(new ScriptBundle("~/Script/CodeMirror").Include(
                 "~/Scripts/codemirror-2.35/lib/codemirror.js",
@@ -60,13 +59,11 @@ namespace JavaProgrammingContest.Web.App_Start {
                 "~/Scripts/application/models/scoreboard.js",
                 "~/Scripts/application/models/settings.js",
                 "~/Scripts/application/models/progress.js",
-
                 "~/Scripts/application/views/application.js",
                 "~/Scripts/application/views/assignments.js",
                 "~/Scripts/application/views/editor.js",
                 "~/Scripts/application/views/console.js",
                 "~/Scripts/application/views/timer.js",
-
                 "~/Scripts/application/helper.js",
                 "~/Scripts/application/builder.js",
                 "~/Scripts/application/application.js"));
@@ -81,7 +78,7 @@ namespace JavaProgrammingContest.Web.App_Start {
 
             // QUnit
             bundles.Add(new StyleBundle("~/Content/QUnit").Include(
-                       "~/Content/qunit.css"));
+                "~/Content/qunit.css"));
             bundles.Add(new ScriptBundle("~/Script/QUnit").Include(
                 "~/Scripts/qunit.js",
                 "~/Scripts/tests/testsuite.js"));
